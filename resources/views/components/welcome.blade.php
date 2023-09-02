e65bbca47a11a3903f96eceb2f8c621a6c183137
Thales Bitencourt <thales@repairpricer.com>
2023-09-02 16:34:12 -0300

    feat: updated welcome message

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -2,14 +2,14 @@
     <x-application-logo class="block h-12 w-auto" />
 
     <h1 class="mt-8 text-2xl font-medium text-gray-900 dark:text-white">
-        Welcome to your Jetstream application!
+        Bem vindo ao Ficha Online!
     </h1>
 
     <p class="mt-6 text-gray-500 dark:text-gray-400 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+        Somente aqui no <b>Ficha Online</b> você pode facilmente criar e manter sua ficha totalmente online podendo acessar
+        a qualquer momento ou qualquer lugar. Além disso é possível também exportar sua ficha para PDF e até mesmo imprimir.
+        Crie os mais variados personagens usando todas as regras oficiais ou criando suas próprias, além de simular suas
+        rolagens de dados dos seus poderes ou habilidade. Espero que vocês gostem! :)
     </p>
 </div>
 
